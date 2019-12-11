ddc7776a6a1407715ce04694dca5a53d5098c3af
Richard-Su <suliquangm@gmail.com>
2019-12-11 11:09:49 +0800

    modify vbtc transfer to mocking test code;

--- ViolasWebservice.py
+++ ViolasWebservice.py
@@ -396,19 +396,28 @@ def GetVBtcTransactionInfo():
     resp["code"] = 2000
     resp["message"] = "ok"
 
-    cli = MakeViolasClient()
-    results = cli.get_transactions(start_version, 10, True)
+    # cli = MakeViolasClient()
+    # results = cli.get_transactions(start_version, 10, True)
 
     infos = []
-    for i in results:
-        info = {}
-        info["sender_address"] = i.raw_txn.type.sender # "f086b6a2348ac502c708ac41d06fe824c91806cabcd5b2b5fa25ae1c50bed3c6"
-        info["sequence_number"] = i.raw_txn.sequence_number
-        info["amount"] = i.raw_txn.type.amount
-        info["version"] = i.version
-        info["btc_address"] = i.events[0].event.data
+    info = {}
+    info["sender_address"] = "f086b6a2348ac502c708ac41d06fe824c91806cabcd5b2b5fa25ae1c50bed3c6"
+    info["sequence_number"] = 1
+    info["amount"] = 10000000
+    info["version"] = 4999
+    info["btc_address"] = "2NGQjMnVhwVVzw1Sq7vjAz9Rf7Z1Fv8LFsV"
+    infos.append(info)
 
-        infos.append(info)
+    # infos = []
+    # for i in results:
+    #     info = {}
+    #     info["sender_address"] = i.raw_txn.type.sender # "f086b6a2348ac502c708ac41d06fe824c91806cabcd5b2b5fa25ae1c50bed3c6"
+    #     info["sequence_number"] = i.raw_txn.sequence_number
+    #     info["amount"] = i.raw_txn.type.amount
+    #     info["version"] = i.version
+    #     info["btc_address"] = i.events[0].event.data
+
+    #     infos.append(info)
 
     resp["data"] = infos
 
@@ -422,23 +431,23 @@ def VerifyVBtcTransactionInfo():
     resp["code"] = 2000
     resp["message"] = "ok"
 
-    cli = MakeViolasClient()
-    result = cli.get_transaction(params.version, True)
+    # cli = MakeViolasClient()
+    # result = cli.get_transaction(params.version, True)
 
-    if result.raw_txn.type.sender != params["sender_address"]:
-        resp["code"] = 2009
-        resp["message"] = "The transaction information is incorrect."
-        return resp
+    # if result.raw_txn.type.sender != params["sender_address"]:
+    #     resp["code"] = 2009
+    #     resp["message"] = "The transaction information is incorrect."
+    #     return resp
 
-    if result.raw_txn.type.amount != params["amount"]:
-        resp["code"] = 2009
-        resp["message"] = "The transaction information is incorrect."
-        return resp
+    # if result.raw_txn.type.amount != params["amount"]:
+    #     resp["code"] = 2009
+    #     resp["message"] = "The transaction information is incorrect."
+    #     return resp
 
-    if result.events[0].event.data != params["btc_address"]:
-        resp["code"] = 2009
-        resp["message"] = "The transaction information is incorrect."
-        return resp
+    # if result.events[0].event.data != params["btc_address"]:
+    #     resp["code"] = 2009
+    #     resp["message"] = "The transaction information is incorrect."
+    #     return resp
 
     return resp
 
