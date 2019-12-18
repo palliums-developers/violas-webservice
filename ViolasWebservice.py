3e1e13fb967eada68788b2d5ced7fef5125af3fb
Richard-Su <suliquangm@gmail.com>
2019-12-18 10:42:23 +0800

    fix bug;

--- ViolasWebservice.py
+++ ViolasWebservice.py
@@ -180,7 +180,7 @@ def MakeLibraTransaction():
         resp["message"] = "Error: Waiting for background response timeout!"
     except TransactionIllegalError as e:
         resp["code"] = 2011
-        resp["message"] = f"Error: {e.message}"
+        resp["message"] = f"Error: {e}"
 
     return resp
 
@@ -210,7 +210,7 @@ def MakeViolasTransaction():
         resp["message"] = "Error: Waiting for background response timeout!"
     except TransactionIllegalError as e:
         resp["code"] = 2011
-        resp["message"] = f"Error: {e.message}"
+        resp["message"] = f"Error: {e}"
 
     return resp
 
