8f949b9c85008c3e218db3d9192fd9fb45712b89
Richard-Su <suliquangm@gmail.com>
2020-09-28 16:37:06 +0800

    update Libra clien version;

--- Deploy.sh
+++ Deploy.sh
@@ -3,7 +3,7 @@
 sudo docker stop violas-webservice
 sudo docker rm violas-webservice
 sudo docker image rm violas-webservice
-sudo docker image build -t violas-webservice .
+sudo docker image build --no-cache -t violas-webservice .
 sudo docker run --name=violas-webservice --rm --network=host -itd violas-webservice python3 CreateTable.py
 sudo docker stop violas-webservice
 sudo docker run --name=violas-webservice --network=host -d violas-webservice
--- Update.sh
+++ Update.sh
@@ -3,5 +3,5 @@
 sudo docker stop violas-webservice
 sudo docker rm violas-webservice
 sudo docker image rm violas-webservice
-sudo docker image build -t violas-webservice .
+sudo docker image build --no-cache -t violas-webservice .
 sudo docker run --name=violas-webservice --network=host -d violas-webservice
